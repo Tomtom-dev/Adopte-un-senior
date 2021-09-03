d0c51dba1258e33dca9dadc5b7f27193609b8c3e
almohelo <almoyner.heloise@gmail.com>
2021-09-03 10:43:00 +0200

    text and img of homepage

---
+++ src/app/accueil-contenu/accueil-contenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil-contenu',
+  templateUrl: './accueil-contenu.component.html',
+  styleUrls: ['./accueil-contenu.component.scss']
+})
+export class AccueilContenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
