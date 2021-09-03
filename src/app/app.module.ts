fe1d3cbabe1f35419775455c31992352d55f2375
Tomtom-dev <54830680+Tomtom-dev@users.noreply.github.com>
2021-09-03 13:34:40 +0200

    add navbar component style it with css and add animation on buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilContenuComponent } from './accueil-contenu/accueil-contenu.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilContenuComponent
+    AccueilContenuComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
