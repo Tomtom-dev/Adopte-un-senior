0a6fe963f874716189f91a57b3d47b5fc7445390
almohelo <almoyner.heloise@gmail.com>
2021-09-03 11:17:05 +0200

    html page qui sommes nous

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuiSommesNousComponent } from './qui-sommes-nous/qui-sommes-nous.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"quisommesnous", component: QuiSommesNousComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AccueilContenuComponent } from './accueil-contenu/accueil-contenu.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccueilContenuComponent } from './accueil-contenu/accueil-contenu.component';
+import { QuiSommesNousComponent } from './qui-sommes-nous/qui-sommes-nous.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilContenuComponent
+    AccueilContenuComponent,
+    QuiSommesNousComponent
   ],
   imports: [
     BrowserModule,
