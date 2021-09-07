a8ac3f4768585902d43a94e2a2ca18de7cfbc327
Guillaume DUBOIS <duboisguillaume806@gmail.com>
2021-09-07 16:29:49 +0200

    add annonce

--- src/app/annonce-form/annonce-form.component.ts
+++ src/app/annonce-form/annonce-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Annonce } from '../shared/models/annonce';
+import { AnnonceService } from '../shared/service/annonce.service';
 
 
 /** @title Form field with error messages */
@@ -12,19 +14,26 @@ export class AnnonceFormComponent {
 
   annonceForm: FormGroup;
 
-  constructor() {
+  constructor(private annonceService: AnnonceService) {
     this.annonceForm = new FormGroup({
       //email = new FormControl('', [Validators.required, Validators.email]);
       type: new FormControl('', Validators.required),
       title: new FormControl('', Validators.required),
       localisation: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required),
-      img: new FormControl('', Validators.required)
+      //img: new FormControl('', Validators.required)
     });
   }
   addAnnonce(){
-    console.log("ok")
+    if(this.annonceForm.status === "VALID"){
+      this.annonceService.addAnnonce(this.annonceForm.value).subscribe((newAnnonce:Annonce)=>{
+        console.log(newAnnonce);
+        this.annonceForm.reset();
+      });
+    }
+    console.log(this.annonceForm);
   }
+  
   getErrorMessage() {
     /* if (this.email.hasError('required')) {
        return 'You must enter a value';
