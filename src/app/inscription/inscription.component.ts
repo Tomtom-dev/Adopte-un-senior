1f64beef6418e9f11f865fbb003ec1553b91cffe
Tomtom-dev <54830680+Tomtom-dev@users.noreply.github.com>
2021-09-10 23:17:12 +0200

    add form and style to the inscription page

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
-  styleUrls: ['./inscription.component.scss']
+  styleUrls: ['./inscription.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
+
+
 export class InscriptionComponent implements OnInit {
 
-  constructor() { }
+  
+  authForm: FormGroup;
+
+  constructor() {
+    this.authForm = new FormGroup({
+      email: new FormControl("", [Validators.email, Validators.required]),
+      password: new FormControl("", Validators.required)
+    })
+   }
 
   ngOnInit(): void {
   }
