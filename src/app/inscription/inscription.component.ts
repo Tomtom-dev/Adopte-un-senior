03978a63a00e248d3d1e531646df4b47a3c15bf7
Tomtom-dev <54830680+Tomtom-dev@users.noreply.github.com>
2021-09-11 13:53:23 +0200

    add slect button and dynamic choice

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+interface Choice {
+  value: string;
+  viewValue: string;
+}
+
+
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
@@ -11,11 +17,17 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 export class InscriptionComponent implements OnInit {
 
+  choices : Choice[]=[
+    {value:"association", viewValue:"Une association"},
+    {value:"benevole", viewValue:"Un bénévole"},
+    {value:"senior", viewValue:"Une personne agée"}
+  ]
   
   authForm: FormGroup;
 
   constructor() {
     this.authForm = new FormGroup({
+      type: new FormControl("", Validators.required),
       nom: new FormControl("", [Validators.email, Validators.required]),
       prenom: new FormControl("", Validators.required),
       mail: new FormControl("", Validators.required),
